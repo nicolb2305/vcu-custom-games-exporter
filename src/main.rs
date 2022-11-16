b688d8ec207036e80cad3d22db4cdf4155763149
nicolb2305 <nico@dsl.no>
2022-11-16 18:43:01 +0100

    Moved api to external crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-#![allow(non_snake_case)]
-mod api;
-use api::api_types::{LolMatchHistoryMatchHistoryList, LolMatchHistoryMatchHistoryGame};
-use shaco;
+use shaco::{rest::RESTClient, api::api_endpoints::{get_lol_match_history_v1_products_lol_current_summoner_matches, get_lol_match_history_v1_games_by_game_id}};
 use std::fs;
 use clap::Parser;
 
@@ -9,7 +6,7 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value="20")]
-    amount: u8
+    amount: u32
 }
 
 #[tokio::main]
@@ -17,23 +14,17 @@ async fn main() {
     // Parse arguments and verify amount of games to fetch
     let args = Args::parse();
 
-    let mut endIndex = args.amount - 1;
-    if endIndex > 199 {
+    let mut end_index = args.amount - 1;
+    if end_index > 199 {
         println!("A maximum of 200 games can be fetched, fetching 200.");
-        endIndex = 199;
+        end_index = 199;
     }
 
-    let client = shaco::rest::RESTClient::new().expect("Failed to create client.");
+    let client = RESTClient::new().expect("Failed to create client.");
 
     // Get match history
-    let endpoint = format!("/lol-match-history/v1/products/lol/current-summoner/matches?endIndex={endIndex}");
 
-    let matches: LolMatchHistoryMatchHistoryList = serde_json::from_value(
-        client
-            .get(endpoint.to_string())
-            .await
-            .expect("Failed to fetch from endpoint.")
-    ).expect("Failed to deserialize match list json");
+    let matches = get_lol_match_history_v1_products_lol_current_summoner_matches(&client, None, Some(end_index)).await.unwrap();
 
     // Create output directory
     fs::create_dir_all("out").expect("Failed to create output directory");
@@ -41,20 +32,14 @@ async fn main() {
     // Find and fetch custom games
     for match_history in matches.games.games {
         // Check for custom games played on Summoner's rift (mapId 11)
-        if (match_history.gameMode == "CLASSIC") & (match_history.gameType == "CUSTOM_GAME") & (match_history.mapId == 11) {
+        if (match_history.game_mode == "CLASSIC") & (match_history.game_type == "CUSTOM_GAME") & (match_history.map_id == 11) {
             // Fetch game and verify that it has 10 players
-            let gameId = match_history.gameId;
-            let endpoint = format!("/lol-match-history/v1/games/{gameId}");
-            let full_match_history: LolMatchHistoryMatchHistoryGame = serde_json::from_value(
-                client
-                    .get(endpoint.to_string())
-                    .await
-                    .expect("Failed to fetch from endpoint.")
-            ).expect("Failed to deserialize match history json");
-            if full_match_history.participantIdentities.len() == 10 {
+            let game_id = match_history.game_id;
+            let full_match_history = get_lol_match_history_v1_games_by_game_id(&client, game_id).await.unwrap();
+            if full_match_history.participant_identities.len() == 10 {
                 // Serialize json object and write string to json file in output folder
                 let full_match_history_string = serde_json::to_string(&full_match_history).expect("Failed to serialize json");
-                fs::write(format!("out/{gameId}.json"), full_match_history_string).expect("Unable to write file");
+                fs::write(format!("out/{game_id}.json"), full_match_history_string).expect("Unable to write file");
             }
         }
     }
